6ae88e982ee314347f08def091895ee097435e53
LauraEllingson <lellingson@ufl.edu>
2024-02-10 16:48:48 -0500

    pseudocode

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ var words = [
   'ukulele',
   'mango'
 ]
+  //To begin: 
+  //chooose random word from list and place in #word-to-guess element
+  //Replace letters of word as underscores
+  //display previous as blank
+  //display incorrect letters blank
+  //display remaining guesses as 10
+  //display wins at 0
+  //display losses at zero
+  //check if letter is in the word
+  //If correct replace respective underscore with letter 
+  //If incorrect underscores remain unchanged
+  //IF non letter is pressed it remains unchanged
+  //If a letter is pressed repeatedly nothing changes
+  //Log incorrect letters in the #incorrect-letters element
+  //update remaining guesses to reflect fewer guesses
+  //If the letters are guessed correctly update the wins to 1
+  //If the user runs out of guesses proceed to the next word and update losses to 1
